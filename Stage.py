90e1b17129868af729ed3138a433c49bf719b1d7
OliviaSeidel <66221237+OliviaSeidel@users.noreply.github.com>
2022-08-02 13:33:33 -0500

    Add some GUI features

--- Stage.py
+++ Stage.py
@@ -3,20 +3,28 @@ from pipython import pitools
 import scipy
 from scipy import optimize
 from scipy.interpolate import interp2d
+import numpy as np
 import time
 
 class Stage:
 
-    X=None
-    Y=None
-    Z=None
+    X=0
+    Y=0
+    Z=0
+    
+    StagePosFile="TmpData/stagepos.txt"
 
     __X_Axis=None
     __Y_Axis=None
     __Z_Axis=None
 
-    def __init__(self,XYZStart=(0,0,3.35)):
+    def __init__(self,LastPos=False,XYZStart=(0,0,3.35)):
         Controller = 'E-873'
+        if(LastPos):
+            f=open(self.StagePosFile,'r')
+            XYZStart= np.loadtxt(f)
+            f.close()
+            
         self.__X_Axis = GCSDevice(Controller)
         self.__Y_Axis = GCSDevice(Controller)
         self.__Z_Axis = GCSDevice(Controller)
@@ -38,29 +46,33 @@ class Stage:
         self.__Y_Axis.SVO( self.__Y_Axis.axes, values=True)
         time.sleep(10)
 
-        self.MoveToX(0) 
+        self.MoveToX(XYZStart[0]) 
         self.__Y_Axis.FNL(self.__Y_Axis.axes)
         
         time.sleep(10)
         
-        if(XYZStart!=None):
-            self.MoveToX( XYZStart[0])
-            self.MoveToY( XYZStart[1])
-            self.MoveToZ( XYZStart[2])
+        self.MoveToY( XYZStart[1])
+        self.MoveToZ( XYZStart[2])
+
 
         self.FocalPlane=None
 
     def MoveToX(self,x):
         self.__X_Axis.MOV(self.__X_Axis.axes, x)
         self.X=x
+        self.SavePos()
 
     def MoveToY(self,y):
         self.__Y_Axis.MOV(self.__Y_Axis.axes, y)
         self.Y=y
+        self.SavePos()
+
 
     def MoveToZ(self,z):
         self.__Z_Axis.MOV(self.__Z_Axis.axes, z)
         self.Z=z
+        self.SavePos()
+
 
     def MoveTo(self,x,y,z):
         self.MoveToX(x)
@@ -70,6 +82,11 @@ class Stage:
     def GetAxes(self):
         return(self.__X_Axis, self.__Y_Axis, self.__Z_Axis)
 
+    def SavePos(self):
+        f=open(self.StagePosFile,'w')
+        ToSave=np.array([self.X,self.Y,self.Z])
+        np.savetxt(f,ToSave)
+        f.close()
     
     def DefineFocalPlane(self,FocalPoints):
         
